f0a52607d2dea942b5b0dbeb17e94967cbe0781b
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-06-02 01:29:44 +0530

    speaker section

---
+++ assets/js/video.js
@@ -0,0 +1,35 @@
+	var vid = document.getElementById("bgvid");
+	var pauseButton = document.querySelector("#polina button");
+
+	function vidFade() {
+	  vid.classList.add("stopfade");
+	}
+
+	vid.addEventListener('ended', function() {
+	// only functional if "loop" is removed 
+	vid.pause();
+	// to capture IE10
+	vidFade();
+	}); 
+	pauseButton.addEventListener("click", function() {
+	  vid.classList.toggle("stopfade");
+	  if (vid.paused) {
+	    vid.play();
+	    pauseButton.innerHTML = "<i class='lni-pause'></i>";
+	  } else {
+	    vid.pause();
+	    pauseButton.innerHTML = "<i class='lni-play'></i>";
+	  }
+	})
+
+	function speaker1(){
+		document.getElementById("guestName").textContent="Manish Agarwal";
+		document.getElementById("guestdesc").textContent="Inauguration Title Sponsor speech Speaker's session Coding round 1 Speaker's session Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto omnis tenetur corrupti officiis, assumenda in excepturi magnam laborum! Quod repellat aperiam, maxime blanditiis esse dignissimos enim ea accusantium ab laudantium.";
+		document.getElementById("speaker-morning").src="/assets/img/speakers/manish agrawal.jpg"
+	  }
+	  function speaker2(){
+		document.getElementById("guestName").textContent="Akash Dixit";
+		document.getElementById("guestdesc").textContent=`Robotics Engineer & Data Scientist, with 6 Years of experience in the Robotics Industry and delivering AI-based real-time solutions for 
+		humanoid robots. Developed humanoid Robot for various educational & Service platforms, recently completed MTech from BITS Pilani in Data Science.`;
+		document.getElementById("speaker-morning").src="/assets/img/speakers/akash dixit.jpg"
+	  }
